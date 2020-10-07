cfbf7fdee669f20c6b831419e46963a7db87d6cc
LemonskyChara <71406789+LemonskyChara@users.noreply.github.com>
2020-10-07 12:34:10 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,22 +1,11 @@
 #!python3
 
+def sum(a,b):
+    answer = a + b
+    return answer
 
-    # a : float 
-    # b : float
-    # return value: returns the sum of the 2 numbers
+c = float(input("Enter a number: "))
+d = float(input("Enter a number: "))
 
-
-
-#this should return a value of 7
-x = sum(3,4)
-
-#this should return a value of 12.5
-y = sum(11,1.5)
-
-
-    
-print(sum)
-
-
-a = float(input("Enter a number: "))
-b = float(input("Enter a number: "))
+x = sum(c,d)
+print(x)
