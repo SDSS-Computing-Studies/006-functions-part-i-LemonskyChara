9804cd440932f5738184164ef9852fe3de15fd11
LemonskyChara <71406789+LemonskyChara@users.noreply.github.com>
2020-10-07 13:01:35 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,3 +6,13 @@ The input is a list.
 The return value is the largest value in the list
 (2 points)
 """
+
+def largest(numlist):
+    a = numlist[0]
+    for i in numlist:
+        if i > a:
+            a = i
+    return a
+numberlist = [5,1,12.3]
+b = largest(numberlist)
+print(b)
