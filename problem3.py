c504f04ca5a0141a9976c9a86c2ea78f46e00ddc
LemonskyChara <71406789+LemonskyChara@users.noreply.github.com>
2020-10-08 11:57:13 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -8,3 +8,11 @@ into another number.
 Example: The factors of 10 are 1, 2, 5, 10
 (2 points)
 """
+numlist = []
+def factors(a):
+    for i in range(1,a+1):
+        if a % i == 0:
+            numlist.append(i)
+    b = ("The factors of " + str(a) + " are " + str(numlist))
+
+    return b
